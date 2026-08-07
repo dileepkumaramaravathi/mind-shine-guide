960cd28481cc58010e8b7416e6cde95911306168
dileepkumaramaravathi <dileepkumaramaravathi213198@gmail.com>
2026-08-07 09:18:48 +0530

    clean-up-root-files-to-separate-web-and-app-folders

--- tools/set_remote_ssh.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-cwd = r'C:/Users/dileep/Downloads/mind-mood-app-main/mind-mood-app-main'
-cmd = ['git','remote','set-url','origin','git@github.com:dileepkumaramaravathi/mind-mood-app.git']
-p = subprocess.run(cmd, cwd=cwd, capture_output=True, text=True)
-print('RC', p.returncode)
-print(p.stdout)
-print(p.stderr)
